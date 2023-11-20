89468f934c3056ae534d95ce2370e44c19a1b103
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-20 20:01:53 +0900

    fix :: springboot test 주석처리

--- tree/src/test/java/com/chr/tree/TreeApplicationTests.java
+++ tree/src/test/java/com/chr/tree/TreeApplicationTests.java
@@ -1,9 +1,8 @@
 package com.chr.tree;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TreeApplicationTests {
 
 	@Test
