9151495513fb410539d08957112b3f8b85be42dd
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-20 23:25:00 +0900

    fix :: ac review

--- src/main/java/com/chr/tree/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/chr/tree/global/security/auth/AuthDetailsService.java
@@ -1,5 +1,6 @@
 package com.chr.tree.global.security.auth;
 
+import com.chr.tree.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,8 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        // TODO: CUSTOM EXCEPTION
+        return (UserDetails) userRepository.findByEmail(email).orElseThrow(RuntimeException::new);
     }
 }
