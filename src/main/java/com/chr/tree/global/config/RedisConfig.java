9bba6bc75d67d8269e6cbb2cbe95214b03376694
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-25 14:12:10 +0900

    add :: redis config

---
+++ src/main/java/com/chr/tree/global/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.chr.tree.global.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@NoArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+
+    @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
+    RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+}
