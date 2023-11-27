d6ebd660b19f22ab699ce7d70323c7c732b9387b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-27 13:50:39 +0900

    update :: exception about create

--- src/main/java/com/chr/tree/global/error/ErrorCode.java
+++ src/main/java/com/chr/tree/global/error/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     //COMMENT
     NOT_ALLOWED_TYPE("not allowed comment type", 400),
     NOT_FOUND_COMMENT("not found comment", 404),
+    NOT_COMMENT_MYSELF("not comment myself", 403),
     ;
 
     private final String message;
