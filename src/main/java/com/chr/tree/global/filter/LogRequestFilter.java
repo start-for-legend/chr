04419ff2d90d6bea53139f4d2ccf1598dcf46f76
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-28 16:53:15 +0900

    add :: log request without swagger

---
+++ src/main/java/com/chr/tree/global/filter/LogRequestFilter.java
@@ -0,0 +1,33 @@
+package com.chr.tree.global.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LogRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String reURI = request.getRequestURI();
+        if (!reURI.contains("swagger") || !reURI.contains("api-docs")) {
+            log.info("url = " + request.getRequestURI());
+
+            log.info("remote = " + request.getRemoteUser());
+
+            log.info("method = " + request.getMethod());
+        }
+        filterChain.doFilter(request, response);
+    }
+}
