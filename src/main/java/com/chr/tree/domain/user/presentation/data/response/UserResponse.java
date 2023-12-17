24ffc8b2d947bde77d1d74d39d1d2e74873f184f
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-12-18 08:47:24 +0900

    add :: user serach

---
+++ src/main/java/com/chr/tree/domain/user/presentation/data/response/UserResponse.java
@@ -0,0 +1,15 @@
+package com.chr.tree.domain.user.presentation.data.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String userName;
+}
